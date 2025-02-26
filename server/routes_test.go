b96ba6bb16a76b3429b3052ea14939720c17c5b7
devkaare <kaarehofferdal@gmail.com>
2025-02-26 19:05:47 +0100

    Feat

--- server/routes_test.go
+++ server/routes_test.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 )
 
@@ -17,9 +15,6 @@ var (
 	err     error
 	respRec *httptest.ResponseRecorder
 	db      *sql.DB
-
-	port       = 3000
-	testUserID = 12
 )
 
 func setup() {
@@ -60,123 +55,4 @@ func TestHealth(t *testing.T) {
 	}
 
 	fmt.Println(string(data))
-}
-
-func TestGetUsers(t *testing.T) {
-	setup()
-
-	req, err = http.NewRequest("GET", "/users", nil)
-	if err != nil {
-		t.Fatalf("TestGetUsers: %v", err)
-	}
-
-	r.ServeHTTP(respRec, req)
-
-	if respRec.Code != http.StatusOK {
-		t.Fatalf("TestGetUsers: \"expected: %v, received: %v\"", http.StatusOK, respRec.Code)
-	}
-
-	result := respRec.Result().Body
-	data, err := io.ReadAll(result)
-	if err != nil {
-		t.Fatalf("TestGetUsers: %v", err)
-	}
-
-	fmt.Println(string(data))
-}
-
-func TestGetUserByUserID(t *testing.T) {
-	setup()
-
-	req, err = http.NewRequest("GET", fmt.Sprintf("/users/%d", testUserID), nil)
-	if err != nil {
-		t.Fatalf("TestGetUserByUserID: %v", err)
-	}
-
-	r.ServeHTTP(respRec, req)
-
-	if respRec.Code != http.StatusOK {
-		t.Fatalf("TestGetUserByUserID: \"expected: %v, received: %v\"", http.StatusOK, respRec.Code)
-	}
-
-	result := respRec.Result().Body
-	data, err := io.ReadAll(result)
-	if err != nil {
-		t.Fatalf("TestGetUserByUserID: %v", err)
-	}
-
-	fmt.Println(string(data))
-}
-
-func TestCreateUser(t *testing.T) {
-	setup()
-
-	apiUrl := fmt.Sprintf("http://localhost:%d", port)
-	resource := "/users/"
-	rawData := url.Values{}
-	rawData.Set("email", "willsmithspersonalemail@gmail.com")
-	rawData.Set("password", "supersecret123")
-
-	u, _ := url.ParseRequestURI(apiUrl)
-	u.Path = resource
-	urlStr := u.String()
-
-	fmt.Println(strings.NewReader(rawData.Encode()))
-	req, err := http.NewRequest("POST", urlStr, strings.NewReader(rawData.Encode()))
-	if err != nil {
-		t.Fatalf("TestCreateUser: %v", err)
-	}
-
-	// req.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	r.ServeHTTP(respRec, req)
-
-	fmt.Println(respRec.Result().Status)
-
-	TestGetUsers(t)
-}
-
-func TestUpdateUser(t *testing.T) {
-	setup()
-
-	apiUrl := fmt.Sprintf("http://localhost:%d", port)
-	resource := fmt.Sprintf("/users/%d", testUserID)
-	rawData := url.Values{}
-	rawData.Set("email", "coolwillsmith@gmail.com")
-	rawData.Set("password", "supersecret123")
-
-	u, _ := url.ParseRequestURI(apiUrl)
-	u.Path = resource
-	urlStr := u.String()
-
-	fmt.Println(strings.NewReader(rawData.Encode()))
-	req, err := http.NewRequest("PUT", urlStr, strings.NewReader(rawData.Encode()))
-	if err != nil {
-		t.Fatalf("TestUpdateUser: %v", err)
-	}
-
-	// req.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	r.ServeHTTP(respRec, req)
-
-	fmt.Println(respRec.Result().Status)
-
-	TestGetUsers(t)
-}
-
-func TestDeleteUser(t *testing.T) {
-	setup()
-
-	req, err = http.NewRequest("DELETE", fmt.Sprintf("/users/%d", testUserID), nil)
-	if err != nil {
-		t.Fatalf("TestDeleteUser: %v", err)
-	}
-
-	r.ServeHTTP(respRec, req)
-
-	fmt.Println(respRec.Result().Status)
-
-	TestGetUsers(t)
 }
